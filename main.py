64794cf9625fd5c7a554a36ace3f23c604a62ab2
krptstc <krptstc@seznam.cz>
2021-05-01 17:28:06 +0200

    Clearing Screen and Getting Input

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from modules.functions import *
 from modules.menus     import *
 
 if __name__ == '__main__':
+    clear_screen()
     print_game_title()
     view_menu(main_menu)
+    pick_from_menu(main_menu)
--- modules/functions.py
+++ modules/functions.py
@@ -1,4 +1,12 @@
+import os
+
 from modules.settings import *
+
+def clear_screen():
+    os.system('clear')
+
+def print_game_title():
+    print(f'{game_title} {game_version}')
 
 def view_menu(menu):
     print('')
@@ -6,5 +14,10 @@ def view_menu(menu):
         print(f'{i + 1}) {menu[i]}')
     print('')
 
-def print_game_title():
-    print(f'{game_title} {game_version}')
+def pick_from_menu(menu):
+    selection = 0
+    while selection < 1 or selection > len(menu):
+        try:
+            selection = int(input('>> '))
+        except:
+            pass
